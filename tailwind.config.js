db2d91ca0e70423d719b0a87603fdfa4721395ac
gitofaman <amanofficial4252@gmail.com>
2024-12-02 11:41:10 +0530

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
